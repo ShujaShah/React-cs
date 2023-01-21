ad86b0c824cc432afa07f41379263599ab91d45a
shuja-shah3 <shuja.shah@iquasar.com>
2023-01-21 21:43:50 +0530

    Starter

--- src/App.js
+++ src/App.js
@@ -1,25 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+
+function Header() {
+  return <h1>This is the header</h1>;
+}
+
+function Footer() {
+  return <h2>This is the footer</h2>;
+}
+
+function Body() {
+  return <h2>This is the body</h2>;
+}
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  return <Footer />;
 }
 
 export default App;
