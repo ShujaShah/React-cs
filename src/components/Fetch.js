e380eb075b67153a587eb6aa716b3dff2eb2a792
shuja-shah3 <shuja.shah@iquasar.com>
2023-03-05 22:39:23 +0530

    using fetch to get the data

---
+++ src/components/Fetch.js
@@ -0,0 +1,27 @@
+import { useState, useEffect } from "react";
+
+export default function GetData() {
+  const [btcData, setBtcData] = useState({});
+
+  const fetchData = () => {
+    fetch(`https://api.coindesk.com/v1/bpi/currentprice.json`)
+      .then((response) => response.json())
+      .then((jsonData) => setBtcData(jsonData.bpi.USD))
+      .catch((error) => console.log(error));
+  };
+
+  useEffect(() => {
+    fetchData();
+  }, []);
+
+  return (
+    <>
+      <h1>Current BTC/USD data</h1>
+      <p>Code: {btcData.code}</p>
+      <p>Symbol: {btcData.symbol}</p>
+      <p>Rate: {btcData.rate}</p>
+      <p>Description: {btcData.description}</p>
+      <p>Rate Float: {btcData.rate_float}</p>
+    </>
+  );
+}
